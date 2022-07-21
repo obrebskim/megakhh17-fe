52fe18e9b12958c33731ba1834e5c30d4ece2a0d
Dawid Kordos <dkordos82@wp.pl>
2022-07-22 00:40:44 +0200

    Updated after code review.

--- src/Components/Common/Input/Input.tsx
+++ src/Components/Common/Input/Input.tsx
@@ -2,24 +2,32 @@ import React, {ChangeEvent} from "react";
 import styled from "styled-components";
 
 const StyledInput = styled.input`
-  color: #c2c0c0;
-  background-color: #2f2f2f;
+  width: ${props => props.width};
+  height: ${props => props.height};
+  color: var(--fontColor);
+  background-color: var(--secondaryDark);
   border-radius: 2px;
 `;
 
 interface Props {
     type: string;
     placeholder: string;
-    width: string;
-    height: string;
+    width?: string;
+    height?: string;
+    required?: boolean;
     handleInputChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const Input = (props: Props) => (
-    <StyledInput
-        type={props.type}
-        style={{width: props.width, height: props.height}}
-        placeholder={props.placeholder}
-        onChange={props.handleInputChange}
-    />
-);
+export const Input = (props: Props) => {
+    const {type, placeholder, width = '150px', height = '30px', required = false, handleInputChange} = props;
+
+    return (
+        <StyledInput
+            type={type}
+            style={{width, height}}
+            placeholder={placeholder}
+            required={required}
+            onChange={handleInputChange}
+        />
+    );
+}
