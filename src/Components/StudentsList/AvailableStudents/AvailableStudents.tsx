05d8c59a234ee36f425c95626aae15c1f908f2c8
Maciej <obrebski.maciej@wp.pl>
2022-08-12 16:13:26 +0200

    Create pagination component

--- src/Components/StudentsList/AvailableStudents/AvailableStudents.tsx
+++ src/Components/StudentsList/AvailableStudents/AvailableStudents.tsx
@@ -4,26 +4,41 @@ import StudentsList from "../StudentsList";
 import AvailableStudentItem from "./AvailableStudentItem";
 import fetchData from "../../../utils/fetchData";
 import { StudentsListItemInterface } from "../../../Types/StudentsListItemInterface";
+import Pagination from "../../Common/Pagination/Pagination";
 
 const Container = styled.section``;
 
 function AvailableStudents() {
   const [students, setStudents] = useState<StudentsListItemInterface[]>([]);
+  const [pageNo, setPageNo] = useState<number>(1);
+  const [allPages, setAllPages] = useState<number>(1);
+  const [perPageNo, setPerPageNo] = useState<number>(10);
   useEffect(() => {
     (async () => {
-      const { allStudents } = await fetchData(
-        "http://localhost:3000/students/all/1/10",
+      const { allStudents, totalPages } = await fetchData(
+        `http://localhost:3000/students/all/${pageNo}/${perPageNo}`,
       );
       setStudents(allStudents);
+      setAllPages(totalPages);
     })();
-  }, []);
+  }, [pageNo, perPageNo]);
   return (
     <Container>
-      <StudentsList>
-        {students.map((s) => (
-          <AvailableStudentItem key={s.id} student={s} />
-        ))}
-      </StudentsList>
+      {students.length > 0 ? (
+        <StudentsList>
+          {students.map((s) => (
+            <AvailableStudentItem key={s.id} student={s} />
+          ))}
+        </StudentsList>
+      ) : (
+        <p>Wczytuję</p>
+      )}
+      <Pagination
+        pageNo={pageNo}
+        allPages={allPages}
+        changePage={setPageNo}
+        changePerPage={setPerPageNo}
+      />
     </Container>
   );
 }
